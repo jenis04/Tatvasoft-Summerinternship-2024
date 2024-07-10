5fc5ec050e1b1bd483a9fce9f8fe8dfb40c7bc7b
jenis sakariya <jenissakariya6768@gmail.com>
2024-07-10 21:27:14 +0530

    first commit

---
+++ DAY 7 (CRUD on Mission and Themes)/Authentication/Authentication/AuthContext.cs
@@ -0,0 +1,21 @@
+﻿using Authentication.Entities;
+using Authentication.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authentication
+{
+    public class AuthContext : DbContext
+    {
+
+        public AuthContext(DbContextOptions<AuthContext> options): base(options) 
+        {
+            
+        }
+        public DbSet<MissionDto> Missions{get;set;}
+        public DbSet<User> Users { get; set; }
+        public DbSet<ThemeViewModel> Themes { get; set; }
+
+        public DbSet<MissionSkillDto> MissionSkills { get; set; } 
+    }
+    
+}
